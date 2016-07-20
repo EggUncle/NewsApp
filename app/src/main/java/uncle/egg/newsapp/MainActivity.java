b21d1f225b928d1ab889dad59304825cf9d2b32e
EggUncle <cicadashadow@gmail.com>
2016-07-20 10:25:46 +0800

    20160720
    开始

---
+++ app/src/main/java/uncle/egg/newsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package uncle.egg.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
